a5c13aa0ae38f3fa32dd5ffe0707aa493fe8bfac
MadEasterBunny <40764200+MadEasterBunny@users.noreply.github.com>
2025-10-23 01:48:40 +0900

    Complete Exercises 06~11

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+    
+    let i = num1;
+    let length = num2;
+    
+    if(num1 > num2) {
+        i = num2;
+        length = num1
+    }
+    
 
+    let sum = 0;
+    for(i; i <= length; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
